f2e7638ec4f0c10a90f9a75b7cb1b6784aa1ec94
Rajnish <therajnish135@gmail.com>
2023-12-30 10:58:48 +0530

    added process code

---
+++ Lab_programs/process/chainProcess.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        int pid;
+        pid = fork();
+
+        if (pid > 0)
+        {
+            printf("parent %ld\n", (long)getpid());
+            break;
+        }
+        else
+        {
+            printf("child %ld\n", (long)getpid());
+        }
+    }
+        return 0;
+    
+}
